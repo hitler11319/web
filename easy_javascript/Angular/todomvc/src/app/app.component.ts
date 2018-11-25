880ede3262047fd73dcb28b3ea2d34b690718b76
hitler11319 <hitler11319@gmail.com>
2018-11-25 19:55:16 +0800

    Angular 基本操作加上建置  相關Angular筆記（創作者：創作者：HueiYou）

--- easy_javascript/Angular/todomvc/src/app/app.component.ts
+++ easy_javascript/Angular/todomvc/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// 我們所設定的東西（NewToDo是我們的函式，但是要註意的是)和{}這種之間都要有空白不然會有紅字）
+// 連註解和文字間也要有空白
 export class AppComponent {
   title = 'todomvc';
+  AppTitle = 'My Angular First Web';
+  placeholderText = 'What need to do?';
+  NewToDo(element) {
+      alert(element.value);
+  }
 }
--- easy_javascript/Angular/todomvc/src/app/app.module.ts
+++ easy_javascript/Angular/todomvc/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
