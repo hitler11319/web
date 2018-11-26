29dc762c04811e4509111119a164694986d397a7
hitler11319 <hitler11319@gmail.com>
2018-11-26 19:40:39 +0800

    Angular基礎建置 及 基本操作（類似MVC的概念） => 主要看src裡面的service filter module .ts .html 的檔案即可，和最外面的index.html（還有Pipe）

--- easy_javascript/Angular/todomvc/src/app/state-filter.pipe.ts
+++ easy_javascript/Angular/todomvc/src/app/state-filter.pipe.ts
@@ -1,14 +1,22 @@
 // Filter的概念
 
+/*
+pipe就是轉換格式的概念，詳見https://angular.io/guide/pipes
+
+例如：最常見的就是{{birthday | date}}  把字串轉成日期格式
+{{ birthday | date:"MM/dd/yy" }}
+
+*/
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'stateFilter',
-  pure : false
+  name: 'stateFilter',   // namespace
+  pure : false  // 是否即時更新
 })
 export class StateFilterPipe implements PipeTransform {
   transform(value: any, state?: any): any {
-    // value：接受的回傳值型別 state(args):回傳值 any是型別 最右邊的any是回傳值
+    // value：接受的值 state(args):參數值，可以在後面繼繼,來加入參數值  any是型別 最右邊的any是回傳值
     console.log(value);
     // console.log(args);
     if (state === 'active') {
